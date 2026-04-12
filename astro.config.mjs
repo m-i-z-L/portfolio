c4da53104d57cbb1313b134f83fbf475efe65fce
Yu Sasaki <mizl.develop@gmail.com>
2026-04-12 22:40:24 +0900

    [feat] ワークフロー追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ubow.github.io',
+  site: 'https://m-i-z-L.github.io',
+  base: '/portfolio',
 });
